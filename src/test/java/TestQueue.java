b7fe28e3b04709eb56b3568a0479e963528b223e
rosem <rosem@0_o.centre.edu>
2024-10-22 08:17:11 -0400

    done?

---
+++ src/test/java/TestQueue.java
@@ -0,0 +1,42 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestQueue
+{
+	 Queue<String> B;
+	 Queue<String> A;
+	
+	@BeforeEach
+	void setUp() throws Exception
+	{
+		B = new Queue<String>();
+	}
+	
+	@Test 
+	void testUnderFlowException()
+	{
+		assertThrows(QueueUnderFlowException.class, () ->
+		{
+			B.pop();
+		});
+	}
+	@Test 
+	void testStacks()
+	{
+		A = new Queue<String>();
+		assertTrue(A.isEmpty());
+		A.push("1st");//first
+		assertFalse(A.isEmpty());
+		A.push("2nd");
+		A.push("3rd");
+		assertEquals("1st",A.pop());
+		assertEquals("2nd",A.pop());
+		assertEquals("3rd",A.pop());
+		assertTrue(A.isEmpty());
+		
+	}
+}
+
+
